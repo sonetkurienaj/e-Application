70c4a8404df7c48ac6b605030ba030429129dc33
Sonet <sonetkurienaj@gmail.com>
2022-06-07 04:08:34 -0700

    first commit

---
+++ src/components/SignUp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SignUp.css";
+const SignUp = () => {
+  return (
+    <div className="signup-section">
+      <h3>hi! welcome to our unity</h3>
+
+      <input type="text" placeholder="email-id" className="input-section" />
+      <button className="button">sign-up</button>
+    </div>
+  );
+};
+
+export default SignUp;
